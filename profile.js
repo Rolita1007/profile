3a29d5c8117a4db148b7b066ee2b89bcf7b41349
rolita1007 <rolita1007@gmail.com>
2017-07-21 11:31:32 -0400

    added skills and project img

--- profile.js
+++ profile.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
 
-});
+// function() {
+
+//   paralaxBg = $('div.parallax-bg');
+
+//   documentElement.on('scroll', function() {
+//     var currentScrollPosition = documentElement.scrollTop();
+//     paralaxBg.css('background-postion', '0 ' + -currentScrollPosition/4 + 'px');
+//   });
+// })();
+
