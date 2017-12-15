7f6ecd161f6ab065f8998b28fe72d0af0a68bdf6
rolita1007 <rolita1007@gmail.com>
2017-12-15 12:52:54 -0500

    added the mycurls project

--- profile.js
+++ profile.js
@@ -1,12 +1,1 @@
-$(document).ready(function() {
-
-// function() {
-
-//   paralaxBg = $('div.parallax-bg');
-
-//   documentElement.on('scroll', function() {
-//     var currentScrollPosition = documentElement.scrollTop();
-//     paralaxBg.css('background-postion', '0 ' + -currentScrollPosition/4 + 'px');
-//   });
-// })();
 
